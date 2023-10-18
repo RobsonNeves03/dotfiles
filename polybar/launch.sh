690680fadb6b0360921e31e702ec043936b205be
RobsonNeves03 <robson.gabriel.neves@gmail.com>
2023-10-18 00:48:04 -0300

    Adding i3wm and polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+#polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+
+# Launch bar1 and bar2
+#echo "---" | tee -a /tmp/example.log 
+#polybar example 2>&1 | tee -a /tmp/example.log & disown/
+
+echo "Bars launched..."
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
